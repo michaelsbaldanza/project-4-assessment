e3330f3933c2649a5fdced422a5be9b3fb74e64d
Michael Baldanza <michael.snider.baldanza@gmail.com>
2020-10-04 17:25:24 -0400

    Updates

---
+++ precache-manifest.3f18316c139de7851ab4b46b9f2582f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9733ace9e7edfc1a35a08023a16acaaf",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "8620989a941534e0d0e1",
+    "url": "/project-4-assessment/static/css/main.6bb19efb.chunk.css"
+  },
+  {
+    "revision": "acc314c7a357911f8cd2",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8620989a941534e0d0e1",
+    "url": "/project-4-assessment/static/js/main.33512f9f.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
